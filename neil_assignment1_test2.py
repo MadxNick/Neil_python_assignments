2a5224216faa1594a9e8b26794fe183eb1f2dd2d
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-05 09:27:05 +0530

    Add files via upload

---
+++ neil_assignment1_test2.py
@@ -0,0 +1,10 @@
+#write a program to read 2 integer m and n & swap there value with or without using a temperary variable
+def swap () :
+ print("hello")
+m = (input("enter a value of m :"))
+n = (input("enter a value of n :"))
+temp = m
+m = n
+n = temp
+print ("The Value of m after swapping: ",m)
+print ("The Value of n after swapping: ",n)
