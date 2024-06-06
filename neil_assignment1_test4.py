4dc6ad8012c86d70e6571c23bc7f9b883a2988f0
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-06 08:57:17 +0530

    Add files via upload

---
+++ neil_assignment1_test4.py
@@ -0,0 +1,8 @@
+# in a class , mark of 5 student are given 80 85 70 76 60 . wap to calculate the average and display it.
+studentmarks1 = 80
+studentmarks2 = 85
+studentmarks3 = 70
+studentmarks4 = 76
+studentmarks5 = 60 
+average = ((studentmarks1 + studentmarks2 + studentmarks3 + studentmarks4 + studentmarks5 )/5)
+print (average)
