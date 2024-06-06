f29f0b06df9f0bbeae9f752e00547d62a91304cb
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-06 09:25:13 +0530

    Add files via upload

---
+++ neil_assignment1_test5.py
@@ -0,0 +1,5 @@
+# temp of a city in fahrenheit degree is input through keyboard.write a program to convert this tempreture into centigrade degree
+f =float (input("enter tempreture of city in fahrenheit degree :"))
+c =  float( f - 32) * 5/9
+print("fahrenheit degree =",f)
+print("celsius degree =",c)
