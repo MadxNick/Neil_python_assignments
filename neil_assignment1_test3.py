1a06ea891129158279c81d26136bcac6f45ff8d7
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-06 21:03:43 +0530

    Update neil_assignment1_test3.py

--- neil_assignment1_test3.py
+++ neil_assignment1_test3.py
@@ -7,3 +7,22 @@ print(dearnessallowance)
 print(houserent)
 print(basicsalary)
 print(grosssalary)
+
+
+# Checked but formula applied is incorrect
+# marks obtained 1/5 
+# Below is corrected code
+
+basicsalary = float(input("Enter the basic salary of Ramesh: "))
+# Calculate dearness allowance which is 40% of basic salary
+dearnessallowance = 0.4 * basicsalary # 40 * basicsalary / 100
+# Calculate house rent allowance which is 20% of basic salary
+houserent = 0.2 * basicsalary
+# Calculate gross salary
+grosssalary = basicsalary + dearnessallowance + houserent
+
+# Output the results
+print(f"Dearness Allowance: {dearnessallowance}")
+print(f"House Rent Allowance: {houserent}")
+print(f"Basic Salary: {basicsalary}")
+print(f"Gross Salary: {grosssalary}")
