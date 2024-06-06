f50ee527bf3dd25212b20fa1c60d5012723537b7
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-06 21:07:19 +0530

    Update neil_assignment1_test5.py

--- neil_assignment1_test5.py
+++ neil_assignment1_test5.py
@@ -3,3 +3,9 @@ f =float (input("enter tempreture of city in fahrenheit degree :"))
 c =  float( f - 32) * 5/9
 print("fahrenheit degree =",f)
 print("celsius degree =",c)
+
+# Checked correct 5/5
+# just a better way to write print statement using concept known as f string
+# just write f ahead of " like print(f" then write the content and if want to use any variable write its name in {variable_name} and complete print")
+print(f"fahrenheit degree = {f}")
+print(f"celsius degree = {c}")
