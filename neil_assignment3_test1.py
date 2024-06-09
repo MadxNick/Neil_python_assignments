dbc90b843377e3dde0cf42117c857e72e2eb5295
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-09 10:22:49 +0530

    Add files via upload

---
+++ neil_assignment3_test1.py
@@ -0,0 +1,7 @@
+# write a program to check whether the no is odd or even
+no = int(input("Enter the number: "))
+if (no % 2 ==0) :
+ print ("The number",no, "is even")
+else:
+ print ("The number" ,no, "is odd")
+
