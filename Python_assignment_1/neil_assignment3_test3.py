b3149e58f554212b57f76d4dcaad8b633cf774c2
Neil Madhuwala <madhuwala.neil@gmail.com>
2024-06-11 07:30:07 +0530

    Add files via upload

---
+++ Python_assignment_1/neil_assignment3_test3.py
@@ -0,0 +1,28 @@
+# write a program to print month name corresponding to a digit
+a = int(input("write any number between 1 to 12 :"))
+if a==1:
+    print(a  ,"january")
+elif a==2:
+    print(a  ,"february")
+elif a==3:
+    print(a  ,"march")
+elif a==4:
+    print(a  ,"april")
+elif a==5:
+    print(a  ,"may")
+elif a==6:
+    print(a  ,"june")
+elif a==7:
+    print(a  ,"july")
+elif a==8:
+    print(a  ,"august")
+elif a==9:
+    print(a  ,"september")
+elif a==10:
+    print(a  ,"october")
+elif a==11:
+    print(a  ,"november")
+elif a==12 :
+    print(a  ,"december")
+else :
+    print("invalid input")
